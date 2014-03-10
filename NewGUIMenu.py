1c1edd8498a809e9f8c3ae67f04704d341bab108
Kuda <12060244@brookes.ac.uk>
2014-03-10 16:40:46 +0000

    Update NewGUIMenu.py
    add back button

--- NewGUIMenu.py
+++ NewGUIMenu.py
@@ -1,15 +1,16 @@
 import pygame, Buttons, robot, grid
+#from pygame.locals import*
+#from sys import exit
 
-####
+
+
 
 class PygView():
-
-
 
   
     def __init__(self,height, width, fps):
         """Initialize pygame, window, background, font,...
-        """
+"""
         pygame.init() # define the pygame interface
         pygame.display.set_caption("RoboGuide - Press ESC to quit")
         self._infoObj = pygame.display.Info()
@@ -21,7 +22,7 @@ class PygView():
         self._maze= 0
         self._instructions=0
         self._clock = pygame.time.Clock()
-        self._fps = fps  #Frames per second
+        self._fps = fps #Frames per second
         self._playtime = 0.0
         self._font = pygame.font.SysFont('mono', self._height // 30, bold=True) #set the font for the text
         self._playButton=Buttons.Button() #create an object of Button
@@ -31,9 +32,10 @@ class PygView():
         self._DictionaryButton=Buttons.Button()
         self._HelpButton=Buttons.Button()
         self._RunButton=Buttons.Button()
+        self._backButton=Buttons.Button()
         self._map=0
         self._fpsClock = pygame.time.Clock()
-        self._spriteObj = pygame.image.load("sprite.png").convert_alpha()
+        self._spriteObj = pygame.image.load("bot333.gif").convert_alpha()
 
         self._soundObj = pygame.mixer.Sound("atari.wav")
         self._soundObj2 = pygame.mixer.Sound("computer_move.wav")
@@ -58,7 +60,7 @@ class PygView():
         #if (level1.checkQuestion(newplayerposition[0],newplayerposition[1]) == True):
             ##Activate Question code
         #if (level1.checkEndpoint(newplayerposition[0],newplayerposition[1]) == True):
-            ##Activate end code     
+            ##Activate end code
 
     def rotateRight(self,spriteObj):
             self._robot1.rotateRight()
@@ -72,7 +74,7 @@ class PygView():
 
     def repeatedMove(self,direction,num):
             if direction == "forward":
-                for n in range(1,num) :                
+                for n in range(1,num) :
                     moveForward();
             elif n == "right":
                 for n in range(1,num):
@@ -88,12 +90,14 @@ class PygView():
 
     def run(self):
         """The mainloop
-        """
+"""
         running = True
+        
 
         PygView.SetUpButtonMain(self)
         PygView.SetUpImage(self)
         PygView.SetUpButtonGamePlay(self)
+        
         while running:
 
             self._playerPosition = self._robot1.getPosition()
@@ -109,48 +113,51 @@ class PygView():
             if(self._gamemode == True):
                 self._screen.blit(self._map,(0,0))
                 self._screen.blit(self._spriteObj,(self._playerPosition[0]*(self._infoObj.current_w/22.5),self._playerPosition[1]*(self._infoObj.current_h/10)))
-                ## The 22.5 and 10 values will change depending on the size of the map. 
+                ## The 22.5 and 10 values will change depending on the size of the map.
             
             for event in pygame.event.get():
                
-
                 if event.type == pygame.QUIT:
-                    running = False 
+                    running = False
+                
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    
-                    if self._playButton.pressed(pygame.mouse.get_pos()): 
-                        self._width=569                                                                     # new width for the screen
-                        self._height= 369                                                                    # new height for the screen
-                        self._menu=pygame.transform.scale(self._menu,(0,0))                                  # resizing the menu screen so it disappear
+                    if self._backButton.pressed(pygame.mouse.get_pos()):
+                        PygView.run(self)
+                        
+                    elif self._playButton.pressed(pygame.mouse.get_pos()):
+                        #PygView.run(self)
+                        self._width=569 # new width for the screen
+                        self._height= 369 # new height for the screen
+                        self._menu=pygame.transform.scale(self._menu,(0,0)) # resizing the menu screen so it disappear
                         self._mazeScreen = pygame.display.set_mode((self._width, self._height), pygame.DOUBLEBUF|pygame.FULLSCREEN)
-                        pygame.display.flip()                                                                # display the changes to the menu
+                        pygame.display.flip() # display the changes to the menu
                         self.loadLevel(0)#the number will have to change using a counter
                         #pygame.display.flip()
-                        #pygame.display.update()                                                              # update the change to the screen
+                        #pygame.display.update() # update the change to the screen
                         self._gamemode = True
                         self._menuMode = False
                         self._level1.initialiseGrid()
-
+                                                
                         
-                    elif self._DictionaryButton.pressed(pygame.mouse.get_pos()) :                        
+                    elif self._DictionaryButton.pressed(pygame.mouse.get_pos()) :
                         self._width=569
                         self._height=369
                         self._screen= pygame.display.set_mode((self._width, self._height), pygame.DOUBLEBUF|pygame.FULLSCREEN) # set up a screen with the new width and height
-                        pygame.display.flip()                                                                # display the changes to the menu
+                        pygame.display.flip() # display the changes to the menu
                         self._screen.blit(self._screen,(0,0))
                         self._screen.fill((173,216,230))
                         pygame.display.flip()
-                        pygame.display.update()       
+                        pygame.display.update()
 
 
                     elif self._HelpButton.pressed(pygame.mouse.get_pos()) :
                         self._width=569
                         self._height=369
                         self._screen= pygame.display.set_mode((self._width, self._height), pygame.DOUBLEBUF|pygame.FULLSCREEN) # set up a screen with the new width and height
-                        pygame.display.flip()                                                                # display the changes to the menu
+                        pygame.display.flip() # display the changes to the menu
                         self._screen.blit(self._screen,(0,0))
                         self._screen.fill((173,216,230))
                         pygame.display.flip()
@@ -161,7 +168,7 @@ class PygView():
                         self._width=569
                         self._height=369
                         self._screen= pygame.display.set_mode((self._width, self._height), pygame.DOUBLEBUF|pygame.FULLSCREEN) # set up a screen with the new width and height
-                        pygame.display.flip()                                                                # display the changes to the menu
+                        pygame.display.flip() # display the changes to the menu
                         self._screen.blit(self._screen,(0,0))
                         self._screen.fill((173,216,230))
                         pygame.display.flip()
@@ -175,10 +182,10 @@ class PygView():
                         self._width=900
                         self._height=400
                         self._screen= pygame.display.set_mode((self._width, self._height), pygame.DOUBLEBUF|pygame.FULLSCREEN) # set up a screen with the new width and height
-                        self._menu=pygame.transform.scale(self._menu,(0,0))                                  # resizing the menu screen so it disappear
-                        self._screen.blit(self._menu,(0,0))                                                  # apply the changes with menu to the screen
-                        pygame.display.flip()                                                                # display the changes to the menu
-                        self._screen.blit(self._screen,(0,0))                                                # apply the changes with maze to the screen
+                        self._menu=pygame.transform.scale(self._menu,(0,0)) # resizing the menu screen so it disappear
+                        self._screen.blit(self._menu,(0,0)) # apply the changes with menu to the screen
+                        pygame.display.flip() # display the changes to the menu
+                        self._screen.blit(self._screen,(0,0)) # apply the changes with maze to the screen
                         
                         self._screen.fill((173,216,230))
                         self.draw_text("Instructions", 400,0)
@@ -193,7 +200,10 @@ class PygView():
                         self.draw_text("computer science course. Incorrect answers will result in failure in the level. Once the end point ",20,200)
                         self.draw_text("is reached a final question is asked, a correct answer will allow the player to proceed to the next level",20,220)
                         self.draw_text("where the difficulty of the questions escalate.",20,240)
-                                       
+                        self._backButton.create_button(self._screen,(173,216,230),0,0,100,25,6,'Back',(0,0,0))
+                        if self._backButton.pressed(pygame.mouse.get_pos()):
+                            PygView.run(self)
+                            
                         pygame.display.flip()
                         pygame.display.update()
 
@@ -202,21 +212,25 @@ class PygView():
                     elif self._OptionButton.pressed(pygame.mouse.get_pos()) :
                         self._width=900
                         self._height=400
-                        self._screen= pygame.display.set_mode((self._width, self._height), pygame.DOUBLEBUF) # set up a screen with the new width and height
-                        self._menu=pygame.transform.scale(self._menu,(0,0))                                  # resizing the menu screen so it disappear
-                        self._screen.blit(self._menu,(0,0))                                                  # apply the changes with menu to the screen
-                        pygame.display.flip()                                                                # display the changes to the menu
+                        self._screen= pygame.display.set_mode((self._width, self._height), pygame.DOUBLEBUF|pygame.FULLSCREEN) # set up a screen with the new width and height
+                        self._menu=pygame.transform.scale(self._menu,(0,0)) # resizing the menu screen so it disappear
+                        self._screen.blit(self._menu,(0,0)) # apply the changes with menu to the screen
+                        pygame.display.flip() # display the changes to the menu
                         self._screen.blit(self._screen,(0,0))
                         self._screen.fill((173,216,230))
                         self.draw_text("Option", 450,0)
+                        self._backButton.create_button(self._screen,(173,216,230),0,0,100,25,6,'Back',(0,0,0))
+                        if self._backButton.pressed(pygame.mouse.get_pos()):
+                            PygView.run(self)
+                            
                         pygame.display.flip()
                         pygame.display.update()
 
                 if (event.type==pygame.KEYDOWN):
 
                     if (event.key==pygame.K_LEFT):
-                        self._spriteObj = self.rotateLeft(self._spriteObj)    
-                    elif (event.key==pygame.K_RIGHT): 
+                        self._spriteObj = self.rotateLeft(self._spriteObj)
+                    elif (event.key==pygame.K_RIGHT):
                         self._spriteObj = self.rotateRight(self._spriteObj)
                     elif (event.key==pygame.K_UP):
                         self.moveForward()
@@ -237,7 +251,7 @@ class PygView():
 
     def play_time(self) :
 
-        milliseconds = self._clock.tick(self._fps) 
+        milliseconds = self._clock.tick(self._fps)
         self._playtime += milliseconds / 1000.0
         self.draw_text("PLAYTIME: %6.3f SECONDS" %
                          ( self._playtime),350,0)
@@ -247,7 +261,7 @@ class PygView():
 
     def draw_text(self, text, x, y):
         """Center text in window
-        """
+"""
         fw, fh = self._font.size(text)
         surface = self._font.render(text, True,(0,0,200))
         self._screen.blit(surface, (x,y))
@@ -256,18 +270,19 @@ class PygView():
 
     def SetUpImage(self) :
         """ Set up images for the background
-        """
+"""
         self._menu=pygame.image.load('Concept_Art4.png') # load the image
-        self._menu=pygame.transform.scale(self._menu,(600,400)) # change the size of the image in terms of height and width
+        self._menu=pygame.transform.scale(self._menu,(900,400)) # change the size of the image in terms of height and width
 
         self._maze=pygame.image.load('Concept_Art9.png') # Same as above
         self._maze=pygame.transform.scale(self._maze,(569,369))
 
 
     def SetUpButtonMain(self):
-        self._playButton.create_button(self._screen,(173,216,230),30,95,100,25,6,'Play',(0,0,0))  #surface,colour, x, y, length, height, width, text. text colour
+        self._playButton.create_button(self._screen,(173,216,230),30,95,100,25,6,'Play',(0,0,0)) #surface,colour, x, y, length, height, width, text. text colour
         self._InstructButton.create_button(self._screen,(173,216,230),30,180,155,25,8,'Instructions',(0,0,0))
         self._OptionButton.create_button(self._screen,(173,216,230),30,250,100,20,7,'Options',(0,0,0))
+        self._backButton.create_button(self._screen,(173,216,230),30,200,100,25,6,'Back',(0,0,0))
 
     def SetUpButtonGamePlay(self):
         self._DictionaryButton.create_button(self._mazeScreen,(173,216,255),400,55,160,82,8,'Dictionary',(0,0,0))
@@ -286,3 +301,4 @@ if __name__ == '__main__':
     
     # call with the height, width of window and fps 400,600,30
     PygView(400,600,30).run()
+
